3df17790885225b3348ada5250c142beced04b5e
Aun-Ali-12 <aunali29331@gmail.com>
2025-02-28 07:34:29 +0500

    done

---
+++ app.js
@@ -0,0 +1,27 @@
+let userName;
+
+let myName = "Aun Ali"
+
+let message = "Hello World!"
+
+alert(message) ;
+userName = prompt("What is your name?");
+
+let userAge;
+userAge = prompt("What is your age?");
+
+let userCourse;
+userCourse = prompt("Name of your course you are currently studying?");
+
+alert(userName);
+alert(userAge);
+alert(userCourse);
+alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// let email = "example@gamil.com";
+// alert("My Email is",(email));
+
+let book = "A Smarter way to learn JavaScript";
+alert(book);
+
+document.write("Yeah! i can write a hmtl document through JS.")
