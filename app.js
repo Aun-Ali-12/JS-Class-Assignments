05799f72bc63461ca6b36c6546f90e6e3e12d1c5
Aun-Ali-12 <aunali29331@gmail.com>
2025-02-28 07:37:45 +0500

    done

--- app.js
+++ app.js
@@ -18,10 +18,10 @@ alert(userAge);
 alert(userCourse);
 alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
 
-// let email = "example@gamil.com";
-// alert("My Email is",(email));
+let email = "example@gamil.com";
+alert("My Email is" + email);
 
-let book = "A Smarter way to learn JavaScript";
+let book = "I'm trying to learn JS from book, A Smarter way to learn JavaScript";
 alert(book);
 
 document.write("Yeah! i can write a hmtl document through JS.")
